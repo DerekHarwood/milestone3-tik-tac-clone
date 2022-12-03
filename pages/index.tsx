ba00f8860fb023890e1bb83a6f01b947322b2c5e
CharlotteStrickland <charlottemarie81@gmail.com>
2022-12-03 16:57:07 -0500

    updated index_tsx to show new h1

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      TikTac
-    </div>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   )
 }
 
