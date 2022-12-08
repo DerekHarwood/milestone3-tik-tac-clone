ffc4ebd04476d21ecb87ef118a272c468691e118
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-12-08 14:26:36 -0500

    Working changes for video

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
-import type { NextPage } from 'next'
-import axios from 'axios';
 
-const Home: NextPage = () => {
+import axios from 'axios';
+import { Video } from '../types';
+
+
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
   return (
     <h1 className="text-3xl font-bold underline">
     Hello world!
@@ -10,7 +17,16 @@ const Home: NextPage = () => {
 }
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(); 
+  const {data} = await axios.get('http://localhost:3000/api/post'); 
+
+
+  return {
+    props: {
+      videos: data
+    }
+  }
+
 }
+
 
 export default Home
