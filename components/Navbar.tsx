8adee50e6efa6fa494e25d2847d814646bf17f6b
CharlotteStrickland <charlottemarie81@gmail.com>
2022-12-09 23:54:40 -0500

    Updated Navbar to show success login for Google

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { AiOutlineLogout } from 'react-icons/ai';
-import { GoogleLogin, googleLogout } from '@react-oauth/google'
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import Logo from '../utils/tiktac-logo.png';
 
 const Navbar = () => {
     const user = false;
+
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
             <Link href="/">
@@ -28,8 +29,8 @@ const Navbar = () => {
                 {user ? (
                     <div>Logged In</div>
                 ) : (
-                    <GoogleLogin
-                        onSuccess={(response) => console.log}
+                    <GoogleLogin 
+                        onSuccess={(response) => console.log(response)}
                         onError={() => console.log('Error')}
                     />
                 )}
