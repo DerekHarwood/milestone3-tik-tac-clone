ebeaf3e459b376df3d399a0935c0499caf2fa75e
CharlotteStrickland <charlottemarie81@gmail.com>
2022-12-10 16:44:08 -0500

    updated index.ts under api-post

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -6,17 +6,18 @@ type Data = {
   name: string
 }
 
-export default async function handler( req: NextApiRequest, res: NextApiResponse) {
-  if(req.method === 'GET') {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
     const query = allPostsQuery();
 
     const data = await client.fetch(query);
 
     res.status(200).json(data);
-  } else if(req.method === 'POST') {
+  } else if (req.method === 'POST') {
     const document = req.body;
 
     client.create(document)
-      .then(() => res.status(201).json('Video Created'))
+      .then(() => res.status(201).json('Video Created')
+      );
   }
 }
