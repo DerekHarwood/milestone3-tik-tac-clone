fedf588595b649b67794dbefe34c87e0cbe1b2eb
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-12-10 13:16:26 -0500

    added more styling to upload video section

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -9,7 +9,8 @@ import {client} from '../utils/client';
 
 
 const Upload = () => {
-  const [isLoading, setIsLoading]=useState(false)
+  const [isLoading, setIsLoading]=useState(false);
+  const [videoAsset, setVideoAsset]=useState()
   return (
     <div className="flex w-full h-full">
         <div className="bg-white rounded-lg">
@@ -18,7 +19,7 @@ const Upload = () => {
                     <p className="text-2xl font-bold">Upload Video</p>
                     <p className="text-md text-gray-400mt-1">Post a video to your account</p>
                 </div>
-                <div className="border-dashed rounded-xl 
+                <div className="border-dashed rounded-xl
                 border-4 border-gray-200 flex flex-col
                 justify-center items-center outline-none
                 mt-10 w-[260px] h-[460px] p-10
@@ -28,7 +29,37 @@ const Upload = () => {
                     <p>Uploading...</p>
                   ): (
                     <div>
+                      {videoAsset ? (
+                        <div>
 
+                        </div>
+                      ) : (
+                        <label className="cursor-pointer">
+                          <div className="flex flex-col
+                          items-center justify-center
+                          h-full">
+                            <div className="flex flex-col
+                            items-center justify-center">
+                              <p className="font-bold
+                              text-xl">
+                                <FaCloudUploadAlt
+                                className="text-gray-300
+                                text-6xl"/>
+                              </p>
+                              <p className="text-xl
+                              font-semibold">
+                                Upload Video
+                              </p>
+                            </div>
+                            <p>
+                              MP4 or WebM or ogg <br />
+                              720x1280 or higher <br />
+                              Up to 10 minutes <br />
+                              Less than 2GB
+                            </p>
+                          </div>
+                        </label>
+                      )}
                     </div>
                   )}
                 </div>
@@ -38,4 +69,4 @@ const Upload = () => {
   )
 }
 
-export default upload
+export default Upload
