bcc9784d72f969a1975e005721e8b4a63c701b22
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-12-10 00:04:07 -0500

    submit changes to auth.ts

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -12,7 +12,7 @@ export default async function handler( req: NextApiRequest,
     const query = allPostsQuery();
 
     const data = await client.fetch(query);
-    
+
     res.status(200).json(data);
   }
 }
