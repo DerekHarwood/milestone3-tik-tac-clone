7519792f6a3a0a12f047cb4c39d763edcb636c0b
JCHammer18 <101356353+JCHammer18@users.noreply.github.com>
2022-12-09 21:59:54 -0500

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -39,10 +39,9 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                             <div className="flex items-center gap-2">
                                 <p className='flex gap-2 items-cemter md:text-md font-bold text-promary'>
                                     {post.postedBy.userName} {''}
+                                <GoVerified className="text-blue-400 text-md"/>
                                 </p>
-                                <GoVerified
-                                    className="text-blue-400 text-md" />
-                                <p className="capitalize font-medium txt-xxs text-gray-500 hidden md:block">
+                                <p className="capitalize font-medium txt-xs text-gray-500 hidden md:block">
                                     {post.postedBy.userName}</p>
 
                             </div>
@@ -50,6 +49,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                     </div>
                 </div>
             </div>
+            
             <div className="lg:ml-20 flex gap-4 relative">
                 <div className="rounded-3xl">
                     <Link href="/">
