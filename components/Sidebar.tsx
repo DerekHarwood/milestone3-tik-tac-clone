182b2e706812a36932958dcdf332fae33428333a
CharlotteStrickland <charlottemarie81@gmail.com>
2022-12-11 17:50:52 -0500

    Removed the Login Button from left sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -38,28 +38,6 @@ const Sidebar = () => {
                         </div>
                     </Link>
                 </div>
-                {!userProfile && (
-                    <div className="px-2 py-4 hidden xl:block">
-                        <p className="text-gray-400">Log in to like and comment on videos</p>
-                        <div className="pr-4">
-                            <GoogleLogin
-                                clientId=""
-                                render={(renderProps) => (
-                                    <button
-                                        className="cursor-pointer bg-white text-lg text-[#EC5578] border-[1px] border-[#EC5578] font-semibold px-6 py-3 rounded-md outline-none font-semibold text-[#EC5578] rounded w-full mt-3 hover:text-white hover:bg-[#EC5578]"
-                                        onClick={renderProps.onClick}
-                                        disabled={renderProps.disabled}
-                                    >
-                                        Log in
-                                    </button>
-                                )}
-                                onSuccess={() => {}}
-                                onFailure={() => {}}
-                                cookiePolicy='single_host_origin'
-                            />
-                        </div>
-                    </div>
-                )}
 
                 <Discover />
                 <SuggestedAccounts />
