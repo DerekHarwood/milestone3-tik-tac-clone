aa9cbf42126550cb187b90b8b7f372e34a449922
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-12-12 18:47:30 -0500

    submit changes from pull request

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { client } from '../../utils/client';
-import {allUsersQuery} from '../../utils/queries'
-
-
-export default async function handler(req: NextApiRequest,
-    res: NextApiResponse) {
-    if (req.method === 'GET') {
-     const data = await client.fetch(allUsersQuery());
-
-     if(data) {
-        res.status(200).json(data);
-     } else {
-        res.json([]);
-     }
-
-    }
-}
-=======
 
 import type { NextApiRequest, NextApiResponse } from 'next';
 
@@ -27,7 +7,7 @@ import { client } from '../../utils/client';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if(req.method === 'GET') {
     const data = await client.fetch(allUsersQuery());
-  
+
     if(data) {
       res.status(200).json(data);
     } else {
@@ -35,4 +15,3 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
   } 
 }
->>>>>>> ba883c3da1ca298ee46f55c5a400d759338676c6
