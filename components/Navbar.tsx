74ed3f40d56d2dc5dfe0bc357b2f39569857e384
CharlotteStrickland <charlottemarie81@gmail.com>
2022-12-12 18:44:25 -0500

    Updated Navbar to show Search

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import Router, { useRouter } from 'next/router';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { BiSearch } from 'react-icons/bi';
@@ -10,10 +10,26 @@ import Logo from '../utils/tiktac-logo.png';
 import { createOrGetUser } from '../utils';
 
 import useAuthStore from '../store/authStore';
+import { setSourceMapRange } from 'typescript';
+import { IUser } from '../types';
 
 const Navbar = () => {
-    const user = false;
+    const [user, setUser] = useState<IUser | null>();
+    const [searchValue, setSearchValue] = useState('');
+    const router = useRouter();
     const { userProfile, addUser, removeUser } = useAuthStore();
+    
+    useEffect(() => {
+        setUser(userProfile);
+    }, [userProfile]);
+
+    const handleSearch = (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+
+        if (searchValue) {
+            router.push(`/search/{searchValue}`);
+        }
+    };
 
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
@@ -27,7 +43,25 @@ const Navbar = () => {
                     />
                 </div>
             </Link>
-            <div>SEARCH</div>
+            <div className='relative hidden md:block'>
+                <form
+                    onSubmit={handleSearch}
+                    className='absolute md:static top-10 -left-20 bg-white'
+                >
+                    <input
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                        placeholder='Search accounts and videos'
+                        className='bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:top-0'
+                    />
+                    <button
+                        onClick={handleSearch}
+                        className='absolute md:right-5 right-6 top-4 border-gray-300 pl-4 text-2xl text-gray-400'
+                    >
+                        <BiSearch />
+                    </button>
+                </form>
+            </div>
 
             <div>
                 {userProfile ? (
@@ -36,9 +70,9 @@ const Navbar = () => {
                             <button className="border-2 px-2 md:px-4
                             text-md font-semibold flex items-center
                             gap-2">
-                              <IoMdAdd className="text-xl" /> {` `}
-                              <span className="hidden md:block">Upload</
-                              span>
+                                <IoMdAdd className="text-xl" /> {' '}
+                                <span className="hidden md:block">Upload</
+                                span>
                             </button>
                         </Link>
                         {userProfile.image && (
@@ -69,7 +103,7 @@ const Navbar = () => {
                     <GoogleLogin
                         onSuccess={(response) =>
                             createOrGetUser
-                            (response, addUser)}
+                                (response, addUser)}
                         onError={() => console.log('Error')}
                     />
                 )}
