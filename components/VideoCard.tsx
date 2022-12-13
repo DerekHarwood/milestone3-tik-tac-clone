5102fd57cfbe5dcaf2ca2c3df31438eb3d8b00fa
CharlotteStrickland <charlottemarie81@gmail.com>
2022-12-12 19:41:21 -0500

    Update VideoCard.tsx

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -7,12 +7,11 @@ import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
 import { Video } from './../types';
 
-
-
 interface IProps {
     post: Video;
+    isShowingOnHome?: boolean;
 }
-const VideoCard: NextPage<IProps> = ({ post }) => {
+const VideoCard: NextPage<IProps> = ({ post: { caption, postedBy, video, _id, likes }, isShowingOnHome }) => {
 
     const [isHover, setIsHover] = useState(false);
     const [playing, setPlaying] = useState(false);
@@ -35,18 +34,44 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
        }
     }, [isVideoMuted])
 
+if(!isShowingOnHome) {
+
+    return (
+        <div>
+          <Link href={`/detail/${_id}`}>
+            <video
+              loop
+              src={video.asset.url}
+              className='w-[250px] md:w-full rounded-xl cursor-pointer'
+            ></video>
+          </Link>
+              <div className='flex gap-2 -mt-8 items-center ml-4'>
+                <p className='text-white text-lg font-medium flex gap-1 items-center'>
+                  <BsPlay className='text-2xl' />
+                  {likes?.length || 0}
+                </p>
+              </div>
+          <Link href={`/detail/${_id}`}>
+            <p className='mt-5 text-md text-gray-800 cursor-pointer w-210'>
+              {caption}
+            </p>
+          </Link>
+        </div>
+      )
+    }
+
     return (
         <div className="flex flex-col border-b-2 border-gray-200 pb-6">
             <div>
                 <div className=" flex gap-3 p-2 cursor-pointer font-semibold rounded">
                     <div className="md:w-16 md:h-16 w-10 h-10">
-                        <Link href={`/profile/${post.postedBy._id}`}>
+                        <Link href={`/profile/${postedBy?._id}`}>
                             <>
                                 <Image
                                     width={62}
                                     height={62}
                                     className="rounded-full"
-                                    src={post.postedBy.image}
+                                    src={postedBy.image}
                                     alt="profile photo"
                                     layout="responsive"
                                 />
@@ -56,14 +81,14 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
 
                     </div>
                     <div>
-                        <Link href={`/profile/${post.postedBy._id}`}>
+                        <Link href={`/profile/${postedBy._id}`}>
                             <div className="flex items-center gap-2">
                                 <p className='flex gap-2 items-cemter md:text-md font-bold text-promary'>
-                                    {post.postedBy.userName} {` `}
+                                    {postedBy.userName} {` `}
                                 <GoVerified className="text-blue-400 text-md"/>
                                 </p>
                                 <p className="capitalize font-medium txt-xs text-gray-500 hidden md:block">
-                                    {post.postedBy.userName}</p>
+                                    {postedBy.userName}</p>
                             </div>
                         </Link>
                     </div>
@@ -75,12 +100,12 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
                     onMouseEnter={() => setIsHover(true)}
                     onMouseLeave={() => setIsHover(false)}
                     className="rounded-3xl">
-                    <Link href={`/detail/${post._id}`}>
+                    <Link href={`/detail/${_id}`}>
                         <video
                             loop
                             ref={videoRef}
                             className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
-                            src={post.video.asset.url}>
+                            src={video.asset.url}>
                         </video>
                     </Link>
 
